wentaoli-cn/compose_template

chore: initialize the project

app/src/main/java/com/iloatnew/template/ui/res/Color.kt
@@ -1,0 +1,8 @@
+package com.iloatnew.template.ui.res
+
+import androidx.compose.ui.graphics.Color
+
+object AppColor {
+    val white = Color(0xFFFFFFFF)
+    val black = Color(0xFF000000)
+}

build.gradle.kts
@@ -1,0 +1,7 @@
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.devtools.ksp) apply false
+}

wentaoli-cn/mobile_boilerplate

app/src/main/java/com/iloatnew/composeboilerplate/ui/res/Color.kt
@@ -1,0 +1,8 @@
+package com.iloatnew.composeboilerplate.ui.res
+
+import androidx.compose.ui.graphics.Color
+
+object AppColor {
+    val White = Color(0xFFFFFFFF)
+    val Black = Color(0xFF000000)
+}

build.gradle.kts
@@ -1,0 +1,9 @@
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.google.ksp) apply false
+    alias(libs.plugins.google.gms) apply false
+    alias(libs.plugins.google.crashlytics) apply false
+    alias(libs.plugins.github.versions) apply true // TODO: Remove if unnecessary.
+}